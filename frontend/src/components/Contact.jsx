f285bf9653eb2cd05fc5d75b60ba853826fcffaf
Joseph Anthony Debono <joe@jadebono.com>
2022-04-18 21:54:18 +0200

    changed colour scheme of Contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,17 +12,17 @@ export default function Contact(props) {
 
   // the form field that will be displayed if submission === false
   const form = (
-    <div className="flex justify-center mt-5 mb-20 md:my-10">
-      <div className="flex-col flex justify-center items-center w-4/5 md:w-1/3 bg-blue-50 rounded-2xl border-2 border-blue-700">
-        <div className="my-2 text-xl  uppercase text-blue-700 font-bold">
+    <div className="flex justify-center mt-5 mb-20 md:mb-10">
+      <div className="flex-col flex justify-center items-center w-4/5 md:w-1/3 bg-indigo-50 rounded-2xl border-2 border-indigo-900">
+        <div className="my-2 text-xl  uppercase text-indigo-900 font-bold">
           Contact Us
         </div>
 
-        <div className="my-2 text-center text-sm font-semibold  text-orange-600">
+        <div className="my-2 text-center text-sm font-semibold  text-orange-700">
           We'll get back to you as soon as possible.{" "}
         </div>
         <form
-          className="bg-white w-5/6 m-4 rounded-xl shadow-lg border border-1 border-blue-700 flex flex-col justify-center text-blue-700 font-semibold "
+          className="bg-white w-5/6 m-4 rounded-xl shadow-lg border border-1 border-indigo-900 flex flex-col justify-center text-indigo-900 font-semibold "
           onSubmit={handleSubmit}
         >
           <div className="flex flex-col ml-4 my-2">
@@ -37,7 +37,7 @@ export default function Contact(props) {
               type="text"
               value={fields.name || ""}
               onChange={handleChange}
-              className="ml-4 w-2/3 border border-gray-300 rounded-md shadow-sm p-1"
+              className="ml-4 w-2/3 border border-indigo-900 rounded-md shadow-sm p-1"
             />
           </div>
           <div className="flex flex-col mt-4 ml-4 mb-2">
@@ -52,7 +52,7 @@ export default function Contact(props) {
               type="email"
               value={fields.email || ""}
               onChange={handleChange}
-              className="ml-4 w-2/3 border border-gray-300 rounded-md shadow-sm p-1"
+              className="ml-4 w-2/3 border border-indigo-900 rounded-md shadow-sm p-1"
             />
           </div>
           <div className="flex flex-col mt-4 ml-4 mb-2">
@@ -67,7 +67,7 @@ export default function Contact(props) {
               type="text"
               value={fields.subject || ""}
               onChange={handleChange}
-              className="ml-4 w-2/3 border border-gray-300 rounded-md shadow-sm p-1"
+              className="ml-4 w-2/3 border border-indigo-900 rounded-md shadow-sm p-1"
             />
           </div>
           <div className="flex flex-col mt-4 ml-4 mb-2">
@@ -83,16 +83,18 @@ export default function Contact(props) {
               type="text"
               value={fields.body || ""}
               onChange={handleChange}
-              className="ml-4 w-5/6 border border-gray-300 rounded-md shadow-sm p-1"
+              className="ml-4 w-5/6 border border-indigo-900 rounded-md shadow-sm p-1"
             />
           </div>
           <div className="flex flex-row my-4 justify-around">
-            <button className="btn-form">Submit</button>
+            <button className=" mt-5 w-28 mx-auto border-2 border-indigo-900 bg-indigo-900 text-white rounded-md hover:shadow-2xl hover:text-indigo-900 hover:bg-white transition east-out duration-500">
+              Submit
+            </button>
           </div>
         </form>
-        <p className="mb-4 text-center text-blue-700 font-bold">
+        <p className="mb-4 text-center text-indigo-900 font-bold">
           *{" "}
-          <span className="text-orange-600 font-normal">
+          <span className="text-orange-700 font-normal">
             Required Information
           </span>
         </p>
@@ -103,7 +105,7 @@ export default function Contact(props) {
   // The email confirmation and return home button screen after a successful submission
   const returnHome = (
     <div className="flex justify-center mt-20">
-      <div className="flex flex-col justify-center w-4/12 items-center bg-blue-50 rounded-2xl border-2 border-blue-700">
+      <div className="flex flex-col justify-center w-4/12 items-center bg-blue-50 rounded-2xl border-2 border-indigo-900">
         <div
           className="bg-orange-100 border-l-4 rounded-lg border-orange-500 text-orange-700 p-2 m-4"
           role="alert"
